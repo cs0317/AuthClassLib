7f79b2e1e4f4d6cb161b1ab7366ea998f7d0dca9
shuo <shuo@PROTOAGNOSTIC>
2016-05-05 16:33:11 -0700

    initial commit

---
+++ ClassHierarchy/CST.ts
@@ -0,0 +1,22 @@
+﻿
+    export class CST_MSG {
+        SymT: string = "";
+        SignedBy: string = "";
+        constructor(SymT: string, SignedBy: string) {
+            this.SymT = SymT;
+            this.SignedBy = SignedBy;
+        }
+
+    }
+
+    export class Debug {
+        static reached(): void {
+            console.log("Debug.reached()");
+        }
+    }
+
+    export interface Nondet_Base {
+        Int(): number;
+        String(): string;
+        Bool(): boolean;
+    }
---
+++ ClassHierarchy/app.ts
@@ -0,0 +1,4 @@
+﻿import GenericAuth = require("./GenericAuth");
+var msg = new GenericAuth.M1();
+msg.Realm = "hello";
+console.log(msg.Realm);
