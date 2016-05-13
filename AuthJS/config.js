815252f3417e3d2ff10642763c343b063c6a894e
shuo <shuo@PROTOAGNOSTIC>
2016-05-12 21:00:35 -0700

    merge into one project

---
+++ AuthJS/config.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+var config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+
+if (typeof process.env.PORT == 'undefined') 
+    process.env.PORT = config.AuthJSSettings.port;
+
+config.rootUrl = "http://"+config.WebAppSettings.hostname+':'+ process.env.PORT + '/';
+config.MainPageUrl = "http://" + config.WebAppSettings.hostname + ':' + config.WebAppSettings.port + '/' + "Auth.JS/platforms/aspx/AllInOne.aspx";
+
+module.exports = config;
